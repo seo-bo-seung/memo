2324debac13fdf6f448d4971ad17e4f3389a95c3
seo-bo-seung <sbs7696@naver.com>
2021-12-13 20:03:35 +0900

    .

--- app/src/main/java/com/example/memo/MainActivity.java
+++ app/src/main/java/com/example/memo/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.memo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
+import java.util.Collection;
+import java.util.Iterator;
+
 public class MainActivity extends AppCompatActivity {
 
     private ImageButton btn_plus;
+    private MainAdapter mainAdapter;
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +26,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn_plus = findViewById(R.id.btn_plus);
+        recyclerView = (RecyclerView)findViewById(R.id.rv);
+        linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        mainAdapter = new MainAdapter(MainActivity.this);
+        recyclerView.setAdapter(mainAdapter);
+
+
+        SharedPreferences prefb =getSharedPreferences("memo_contain", MODE_PRIVATE);
+        Collection<?> col_val =  prefb.getAll().values();
+        Iterator<?> it_val = col_val.iterator();
+        Collection<?> col_key = prefb.getAll().keySet();
+        Iterator<?> it_key = col_key.iterator();
+
+
         btn_plus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -24,5 +47,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
     }
 }
